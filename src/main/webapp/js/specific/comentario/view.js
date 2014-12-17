573d53912ff6a383dc8eb82fcb646d3a46a887e4
al037124 <al037124@s09-pc15>
2014-12-17 18:21:58 +0100

    Mantenimiento completo de comentario

--- src/main/webapp/js/specific/comentario/view.js
+++ src/main/webapp/js/specific/comentario/view.js
@@ -110,9 +110,6 @@ comentarioView.prototype.loadButtons = function (id) {
     botonera += '<a class="btn btn-default view" id="' + id + '"  href="jsp#/' + this.clase + '/view/' + id + '"><i class="glyphicon glyphicon-eye-open"></i></a>';
     botonera += '<a class="btn btn-default edit" id="' + id + '"  href="jsp#/' + this.clase + '/edit/' + id + '"><i class="glyphicon glyphicon-pencil"></i></a>';
     botonera += '<a class="btn btn-default remove" id="' + id + '"  href="jsp#/' + this.clase + '/remove/' + id + '"><i class="glyphicon glyphicon-remove"></i></a>';
-    botonera += '<a class="btn btn-default votararriba" id="' + id + '"  href="jsp#/' + this.clase + '/votararriba/' + id + '"><i class="glyphicon glyphicon-arrow-up"></i></a>';
-    botonera += '<a class="btn btn-default votarabajo" id="' + id + '"  href="jsp#/' + this.clase + '/votarabajo/' + id + '"><i class="glyphicon glyphicon-arrow-down"></i></a>';
-    //botonera += '<a class="btn btn-default mp" id="' + id + '"  href="jsp#/' + "mensajeprivado" + '/new/' + id_usuario + '"><i class="glyphicon glyphicon-envelope"></i></a>';
     botonera += '</div></div>';
     return botonera;
 };
