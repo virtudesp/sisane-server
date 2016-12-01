6dbed63bbe65fa511c282f868134819dabb9d508
a003153722p <a003153722p@s09-pc03>
2016-12-01 20:59:53 +0100

    Mantenimiento tabla documento

--- src/main/java/net/daw/bean/implementation/PuserBean.java
+++ src/main/java/net/daw/bean/implementation/PuserBean.java
@@ -1,23 +1,23 @@
 /*
  * Copyright (c) 2016 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
- * 
- * sisane-server: Helps you to develop easily AJAX web applications 
+ *
+ * sisane-server: Helps you to develop easily AJAX web applications
  *                   by copying and modifying this Java Server.
  *
  * Sources at https://github.com/rafaelaznar/sisane-server
- * 
+ *
  * sisane-server is distributed under the MIT License (MIT)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -227,7 +227,6 @@ public class PuserBean implements GenericBean {
 
     @Override
     public String toPairs() {
-
         String strPairs = "";
         //strPairs += "id=" + id + ",";
         strPairs += "name=" + EncodingUtilHelper.quotate(name) + ",";
---
+++ src/main/java/net/daw/dao/implementation/DocumentoDao.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.dao.implementation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import net.daw.bean.implementation.DocumentoBean;
+import net.daw.bean.implementation.PuserBean;
+import net.daw.dao.publicinterface.TableDaoInterface;
+import net.daw.dao.publicinterface.ViewDaoInterface;
+import net.daw.data.implementation.MysqlData;
+import net.daw.helper.statics.FilterBeanHelper;
+import net.daw.helper.statics.Log4j;
+import net.daw.helper.statics.SqlBuilder;
+
+/**
+ *
+ * @author a003153722p
+ */
+public class DocumentoDao implements ViewDaoInterface<DocumentoBean>, TableDaoInterface<DocumentoBean> {
+
+    private String strTable = "documento";
+    private String strSQL = "select * from documento where 1=1 ";
+    private MysqlData oMysql = null;
+    private Connection oConnection = null;
+    private PuserBean oPuserSecurity = null;
+
+    public DocumentoDao(Connection oPooledConnection, PuserBean oPuserBean_security) throws Exception {
+        try {
+            oConnection = oPooledConnection;
+            oMysql = new MysqlData(oConnection);
+            oPuserSecurity = oPuserBean_security;
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+    }
+
+    @Override
+    public Long getCount(ArrayList<FilterBeanHelper> alFilter) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        Long pages = 0L;
+        try {
+            pages = oMysql.getCount(strSQL);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return pages;
+    }
+
+    @Override
+    public ArrayList<DocumentoBean> getPage(int intRegsPerPag, int intPage, ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        strSQL += SqlBuilder.buildSqlLimit(oMysql.getCount(strSQL), intRegsPerPag, intPage);
+        ArrayList<DocumentoBean> arrDoc = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                DocumentoBean oDocumentoBean = new DocumentoBean();
+                arrDoc.add((DocumentoBean) oDocumentoBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrDoc;
+    }
+
+    @Override
+    public ArrayList<DocumentoBean> getAll(ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        ArrayList<DocumentoBean> arrDoc = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                DocumentoBean oDocumentoBean = new DocumentoBean();
+                arrDoc.add((DocumentoBean) oDocumentoBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrDoc;
+    }
+
+    @Override
+    public DocumentoBean get(DocumentoBean oBean, Integer expand) throws Exception {
+        if (oBean.getId() > 0) {
+            ResultSet oResultSet = null;
+            try {
+                oResultSet = oMysql.getAllSQL(strSQL + " And id= " + oBean.getId() + " ");
+                Boolean empty = true;
+                while (oResultSet.next()) {
+                    oBean = (DocumentoBean) oBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                    empty = false;
+                }
+                if (empty) {
+                    oBean.setId(0);
+                }
+            } catch (Exception ex) {
+                Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+                throw new Exception();
+            } finally {
+                if (oResultSet != null) {
+                    oResultSet.close();
+                }
+            }
+        } else {
+            oBean.setId(0);
+        }
+        return oBean;
+    }
+
+    @Override
+    public Integer set(DocumentoBean oBean) throws Exception {
+        Integer iResult = null;
+        try {
+            if (oBean.getId() == 0) {
+                strSQL = "INSERT INTO " + strTable + " ";
+                strSQL += "(" + oBean.getColumns() + ")";
+                strSQL += "VALUES(" + oBean.getValues() + ")";
+                iResult = oMysql.executeInsertSQL(strSQL);
+            } else {
+                strSQL = "UPDATE " + strTable + " ";
+                strSQL += " SET " + oBean.toPairs();
+                strSQL += " WHERE id=" + oBean.getId();
+                iResult = oMysql.executeUpdateSQL(strSQL);
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return iResult;
+    }
+
+    @Override
+    public Integer remove(Integer id) throws Exception {
+        int result = 0;
+        try {
+            result = oMysql.removeOne(id, strTable);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return result;
+    }
+}
