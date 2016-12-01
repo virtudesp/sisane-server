8e335933307e92737346c0804dfda0d118ea6741
a026749314s <a026749314s@s09-pc21>
2016-12-01 20:01:14 +0100

    mantenimiento 'importancia'

---
+++ src/main/java/net/daw/bean/implementation/ImportanciaBean.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean.implementation;
+
+import com.google.gson.annotations.Expose;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import net.daw.bean.publicinterface.GenericBean;
+import net.daw.helper.statics.EncodingUtilHelper;
+
+/**
+ * @author MatarredonaDS
+ */
+
+public class ImportanciaBean implements GenericBean {
+    
+    @Expose
+    private Integer id = 0;
+    @Expose
+    private String descripcion;
+    
+    public ImportanciaBean() {
+    }
+
+    public ImportanciaBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    @Override
+    public String getColumns() {
+        String strColumns = "";
+        strColumns += "id,";
+        strColumns += "descripcion";
+        return strColumns;
+    }
+
+    @Override
+    public String getValues() {
+        String strColumns = "";
+        strColumns += id + ",";
+        strColumns += EncodingUtilHelper.quotate(descripcion);
+        return strColumns;
+    }
+
+    @Override
+    public String toPairs() {
+        String strColumns = "";
+        strColumns += "id=" + id + ",";
+        strColumns += "description=" + EncodingUtilHelper.quotate(descripcion);
+        return strColumns;
+    }
+
+    @Override
+    public ImportanciaBean fill(ResultSet oResultSet, Connection pooledConnection, PuserBean oPuserBean_security, Integer expand) throws SQLException, Exception {
+        this.setId(oResultSet.getInt("id"));
+        this.setDescripcion(oResultSet.getString("descripcion"));
+        
+        return this;
+    }
+    
+}
---
+++ src/main/java/net/daw/dao/implementation/ImportanciaDao.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.dao.implementation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import net.daw.bean.implementation.ImportanciaBean;
+import net.daw.bean.implementation.PuserBean;
+import net.daw.dao.publicinterface.TableDaoInterface;
+import net.daw.dao.publicinterface.ViewDaoInterface;
+import net.daw.data.implementation.MysqlData;
+import net.daw.helper.statics.FilterBeanHelper;
+import net.daw.helper.statics.Log4j;
+import net.daw.helper.statics.SqlBuilder;
+
+/**
+ * @author MatarredonaDS
+ */
+
+public class ImportanciaDao implements ViewDaoInterface<ImportanciaBean>, TableDaoInterface<ImportanciaBean> {
+
+    private String strTable = "importancia";
+    private String strSQL = "select * from importancia where 1=1 ";
+    private MysqlData oMysql = null;
+    private Connection oConnection = null;
+    private PuserBean oPuserSecurity = null;
+    
+    public ImportanciaDao(Connection oPooledConnection, PuserBean oPuserBean_security) throws Exception {
+        try {
+            oConnection = oPooledConnection;
+            oMysql = new MysqlData(oConnection);
+            oPuserSecurity = oPuserBean_security;
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+    }
+    
+    @Override
+    public Long getCount(ArrayList<FilterBeanHelper> hmFilter) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(hmFilter);
+        Long pages = 0L;
+        try {
+            pages = oMysql.getCount(strSQL);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return pages;
+    }
+
+    @Override
+    public ArrayList<ImportanciaBean> getPage(int intRegsPerPag, int intPage, ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        strSQL += SqlBuilder.buildSqlLimit(oMysql.getCount(strSQL), intRegsPerPag, intPage);
+        ArrayList<ImportanciaBean> arrImportancia = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                ImportanciaBean oImportanciaBean = new ImportanciaBean();
+                arrImportancia.add(oImportanciaBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrImportancia;
+    }
+
+    @Override
+    public ArrayList<ImportanciaBean> getAll(ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        ArrayList<ImportanciaBean> arrImportancia = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                ImportanciaBean oImportanciaBean = new ImportanciaBean();
+                arrImportancia.add(oImportanciaBean.fill(oResultSet, oConnection, oPuserSecurity, expand));
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrImportancia;
+    }
+
+    @Override
+    public ImportanciaBean get(ImportanciaBean oImportanciaBean, Integer expand) throws Exception {
+        if (oImportanciaBean.getId() > 0) {
+            ResultSet oResultSet = null;
+            try {
+                oResultSet = oMysql.getAllSQL(strSQL + " And id= " + oImportanciaBean.getId() + " ");
+                Boolean empty = true;
+                while (oResultSet.next()) {
+                    oImportanciaBean = oImportanciaBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                    empty = false;
+                }
+                if (empty) {
+                    oImportanciaBean.setId(0);
+                }
+            } catch (Exception ex) {
+                Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+                throw new Exception();
+            } finally {
+                if (oResultSet != null) {
+                    oResultSet.close();
+                }
+            }
+        } else {
+            oImportanciaBean.setId(0);
+        }
+        return oImportanciaBean;
+    }
+
+    @Override
+    public Integer set(ImportanciaBean oImportanciaBean) throws Exception {
+        Integer iResult = null;
+        try {
+            if (oImportanciaBean.getId() == 0) {
+                strSQL = "INSERT INTO " + strTable + " ";
+                strSQL += "(" + oImportanciaBean.getColumns() + ")";
+                strSQL += "VALUES(" + oImportanciaBean.getValues() + ")";
+                iResult = oMysql.executeInsertSQL(strSQL);
+            } else {
+                strSQL = "UPDATE " + strTable + " ";
+                strSQL += " SET " + oImportanciaBean.toPairs();
+                strSQL += " WHERE id=" + oImportanciaBean.getId();
+                iResult = oMysql.executeUpdateSQL(strSQL);
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return iResult;
+    }
+
+    @Override
+    public Integer remove(Integer id) throws Exception {
+        int result = 0;
+        try {
+            result = oMysql.removeOne(id, strTable);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return result;
+    }
+
+}
