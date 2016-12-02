48288a4800c093db9acd6f48d440b9bfd316ad68
al038557 <al038557@s09-pc18>
2016-12-02 20:13:10 +0100

    Especialidad hecha

---
+++ src/main/java/net/daw/bean/implementation/EspecialidadBean.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2016 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
+ * 
+ * sisane-server: Helps you to develop easily AJAX web applications 
+ *                   by copying and modifying this Java Server.
+ *
+ * Sources at https://github.com/rafaelaznar/sisane-server
+ * 
+ * sisane-server is distributed under the MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.daw.bean.implementation;
+
+import com.google.gson.annotations.Expose;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import net.daw.bean.publicinterface.GenericBean;
+import net.daw.dao.implementation.DocumenttypeDao;
+import net.daw.dao.implementation.UserDao;
+import net.daw.helper.statics.EncodingUtilHelper;
+
+public class EspecialidadBean implements GenericBean {
+
+    @Expose
+    private Integer id = 0;
+    @Expose
+    private String descripcion;
+   
+    public EspecialidadBean() {
+    }
+
+    public EspecialidadBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+   
+
+    @Override
+    public String getColumns() {
+        String strColumns = "";
+        strColumns += "id,";
+        strColumns += "descripcion";
+        return strColumns;
+    }
+
+    @Override
+    public String getValues() {
+        String strColumns = "";
+        strColumns += id + ",";
+        strColumns += EncodingUtilHelper.quotate(descripcion);
+        return strColumns;
+    }
+
+    @Override
+    public String toPairs() {
+        String strColumns = "";
+        //strColumns +="id="; id + ";
+        strColumns += "descripcion=" + EncodingUtilHelper.quotate(descripcion);
+        return strColumns;
+    }
+
+    @Override
+    public EspecialidadBean fill(ResultSet oResultSet, Connection pooledConnection, PuserBean oPuserBean_security, Integer expand) throws SQLException, Exception {
+        this.setId(oResultSet.getInt("id"));
+        this.setDescripcion(oResultSet.getString("descripcion"));
+
+   
+        return this;
+    }
+
+}
