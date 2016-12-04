180238fd7a46f382689633ff0186355021a393c3
nesbarse <barrenosegarra2@gmail.com>
2016-12-04 12:01:54 +0100

    Created Tipomuestra server

---
+++ src/main/java/net/daw/dao/implementation/TipomuestraDao.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.dao.implementation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import net.daw.bean.implementation.PusuarioBean;
+import net.daw.bean.implementation.TipomuestraBean;
+import net.daw.dao.publicinterface.TableDaoInterface;
+import net.daw.dao.publicinterface.ViewDaoInterface;
+import net.daw.data.implementation.MysqlData;
+import net.daw.helper.statics.FilterBeanHelper;
+import net.daw.helper.statics.Log4j;
+import net.daw.helper.statics.SqlBuilder;
+
+/**
+ *
+ * @author a022595832b
+ */
+public class TipomuestraDao implements ViewDaoInterface<TipomuestraBean>, TableDaoInterface<TipomuestraBean>{
+    
+    private String strTable = "tecnica";
+    private String strSQL = "select * from tecnica where 1=1 ";
+    private MysqlData oMysql = null;
+    private Connection oConnection = null;
+    private PusuarioBean oPusuarioSecurity = null;
+
+    public TipomuestraDao(Connection oPooledConnection, PusuarioBean oPusuarioBean_security) throws Exception {
+        try {
+            oConnection = oPooledConnection;
+            oMysql = new MysqlData(oConnection);
+            oPusuarioSecurity = oPusuarioBean_security;
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+    }
+
+    @Override
+    public Long getCount(ArrayList<FilterBeanHelper> alFilter) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        Long pages = 0L;
+        try {
+            pages = oMysql.getCount(strSQL);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return pages;
+    }
+
+    @Override
+    public ArrayList<TipomuestraBean> getPage(int intRegsPerPag, int intPage, ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        strSQL += SqlBuilder.buildSqlLimit(oMysql.getCount(strSQL), intRegsPerPag, intPage);
+        ArrayList<TipomuestraBean> arrTipomuestra = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                TipomuestraBean oTipomuestraBean = new TipomuestraBean();
+                arrTipomuestra.add((TipomuestraBean) oTipomuestraBean.fill(oResultSet, oConnection, oPusuarioSecurity, expand));
+            }
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrTipomuestra;
+    }
+
+    @Override
+    public ArrayList<TipomuestraBean> getAll(ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder, Integer expand) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        strSQL += SqlBuilder.buildSqlWhere(alFilter);
+        strSQL += SqlBuilder.buildSqlOrder(hmOrder);
+        ArrayList<TipomuestraBean> arrTipomuestra = new ArrayList<>();
+        ResultSet oResultSet = null;
+        try {
+            oResultSet = oMysql.getAllSQL(strSQL);
+            while (oResultSet.next()) {
+                TipomuestraBean oTipomuestraBean = new TipomuestraBean();
+                arrTipomuestra.add((TipomuestraBean) oTipomuestraBean.fill(oResultSet, oConnection, oPusuarioSecurity, expand));
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+        }
+        return arrTipomuestra;
+    }
+
+    @Override
+    public TipomuestraBean get(TipomuestraBean oTipomuestraBean, Integer expand) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        if (oTipomuestraBean.getId() > 0) {
+            ResultSet oResultSet = null;
+            try {
+                oResultSet = oMysql.getAllSQL(strSQL + " And id= " + oTipomuestraBean.getId() + " ");
+                Boolean empty = true;
+                while (oResultSet.next()) {
+                    oTipomuestraBean = (TipomuestraBean) oTipomuestraBean.fill(oResultSet, oConnection, oPusuarioSecurity, expand);
+                    empty = false;
+                }
+                if (empty) {
+                    oTipomuestraBean.setId(0);
+                }
+            } catch (Exception ex) {
+                Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+                throw new Exception();
+            } finally {
+                if (oResultSet != null) {
+                    oResultSet.close();
+                }
+            }
+        } else {
+            oTipomuestraBean.setId(0);
+        }
+        return oTipomuestraBean;
+    }
+
+    @Override
+    public Integer set(TipomuestraBean oTipomuestraBean) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        Integer iResult = null;
+        try {
+            if (oTipomuestraBean.getId() == 0) {
+                strSQL = "INSERT INTO " + strTable + " ";
+                strSQL += "(" + oTipomuestraBean.getColumns() + ")";
+                strSQL += "VALUES(" + oTipomuestraBean.getValues() + ")";
+                iResult = oMysql.executeInsertSQL(strSQL);
+            } else {
+                strSQL = "UPDATE " + strTable + " ";
+                strSQL += " SET " + oTipomuestraBean.toPairs();
+                strSQL += " WHERE id=" + oTipomuestraBean.getId();
+                iResult = oMysql.executeUpdateSQL(strSQL);
+            }
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return iResult;
+    }
+
+    @Override
+    public Integer remove(Integer id) throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        int result = 0;
+        try {
+            result = oMysql.removeOne(id, strTable);
+        } catch (Exception ex) {
+            Log4j.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            throw new Exception();
+        }
+        return result;
+    }
+    
+}
