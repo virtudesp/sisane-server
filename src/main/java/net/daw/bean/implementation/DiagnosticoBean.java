3ddb5805a5e9e33e75fef2196587deca775d9096
a022583435b <a022583435b@s09-pc01>
2016-12-05 16:23:45 +0100

    Manto Anticoagulante

--- src/main/java/net/daw/bean/implementation/DiagnosticoBean.java
+++ src/main/java/net/daw/bean/implementation/DiagnosticoBean.java
@@ -28,13 +28,12 @@ public class DiagnosticoBean implements GenericBean{
     private Date fecha;
     @Expose(serialize = false)
     private int id_episodio;
-//    @Expose(deserialize = false)
-//    private EpisodioBean obj_episodio;
+    /*@Expose(deserialize = false)
+    private EpisodioBean obj_episodio;
     @Expose(serialize = false)
-    private int id_tipodiagnostico;
+    private int id_tipodiagnostico;*/
 //    @Expose(deserialize = false)
 //    private TipodiagnosticoBean obj_tipodiagnostico;
-    
     
     public DiagnosticoBean(int id){
         this.id = id;
@@ -75,6 +74,15 @@ public class DiagnosticoBean implements GenericBean{
         this.id_episodio = id_episodio;
     }
 
+
+//    public int getId_tipodiagnostico() {
+//        return id_tipodiagnostico;
+//    }
+
+//    public void setId_tipodiagnostico(int id_tipodiagnostico) {
+//        this.id_tipodiagnostico = id_tipodiagnostico;
+//    }
+
 //    public EpisodioBean getObj_episodio() {
 //        return obj_episodio;
 //    }
@@ -82,23 +90,7 @@ public class DiagnosticoBean implements GenericBean{
 //    public void setObj_episodio(EpisodioBean obj_episodio) {
 //        this.obj_episodio = obj_episodio;
 //    }
-
-    public int getId_tipodiagnostico() {
-        return id_tipodiagnostico;
-    }
-
-    public void setId_tipodiagnostico(int id_tipodiagnostico) {
-        this.id_tipodiagnostico = id_tipodiagnostico;
-    }
-
-//    public TipodiagnosticoBean getObj_tipodiagnostico() {
-//        return obj_tipodiagnostico;
-//    }
-//
-//    public void setObj_tipodiagnostico(TipodiagnosticoBean obj_tipodiagnostico) {
-//        this.obj_tipodiagnostico = obj_tipodiagnostico;
-//    }
-        
+    
     @Override
     public String getColumns() {
         return "id,informe,fecha,id_episodio,id_tipodiagnostico";
@@ -110,8 +102,8 @@ public class DiagnosticoBean implements GenericBean{
         values  = id + ",";
         values += EncodingUtilHelper.quotate(informe) + ",";
         values += EncodingUtilHelper.stringifyAndQuotate(fecha) + ",";
-        values += id_episodio + ",";
-        values += id_tipodiagnostico;
+        values += id_episodio;/* + ",";
+        values += id_tipodiagnostico;*/
         
         return values;
     }
@@ -122,8 +114,8 @@ public class DiagnosticoBean implements GenericBean{
         pairs  = "id = " + id + ",";
         pairs += "informe = " + EncodingUtilHelper.quotate(informe) + ",";
         pairs += "fecha = " + EncodingUtilHelper.stringifyAndQuotate(fecha) + ",";
-        pairs  = "id_episodio = " + id_episodio + ",";
-        pairs  = "id_tipodiagnostico = " + id_tipodiagnostico;
+        pairs  = "id_episodio = " + id_episodio;/* + ",";
+        pairs  = "id_tipodiagnostico = " + id_tipodiagnostico;*/
         
         return pairs;
     }
@@ -134,20 +126,16 @@ public class DiagnosticoBean implements GenericBean{
         this.informe = oResultSet.getString("informe");
         this.fecha = oResultSet.getDate("fecha");
         this.id_episodio = oResultSet.getInt("id_episodio");
-        this.id_tipodiagnostico = oResultSet.getInt("id_tipodiagnostico");
+//        this.id_tipodiagnostico = oResultSet.getInt("id_tipodiagnostico");
         
-//        if(expand > 0){
-//            EpisodioDao eDao = new EpisodioDao(pooledConnection,oPusuarioBean_security);
-//            EpisodioBean eBean = new EpisodioBean();
-//            eBean.setId(this.id_episodio);
-//            this.obj_episodio = eDao.get(eBean,expand - 1);
-//            
-//            TipodiagnosticoDao tDao = new TipodiagnosticoDao(pooledConnection,oPusuarioBean_security);
-//            TipodiagnosticoBean tBean = new TipodiagnosticoBean();
-//            tBean.setId(this.id_tipodiagnostico);
-//            this.obj_tipodiagnostico = tDao.get(tBean,expand - 1);
-//        }
-        
+//    public TipodiagnosticoBean getObj_tipodiagnostico() {
+//        return obj_tipodiagnostico;
+//    }
+//
+//    public void setObj_tipodiagnostico(TipodiagnosticoBean obj_tipodiagnostico) {
+//        this.obj_tipodiagnostico = obj_tipodiagnostico;
+//    }
+
         return this;        
     }
     
---
+++ src/main/java/net/daw/bean/implementation/ViaBean.java
@@ -0,0 +1,121 @@
+
+package net.daw.bean.implementation;
+
+import com.google.gson.annotations.Expose;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import net.daw.bean.publicinterface.GenericBean;
+import net.daw.dao.implementation.MedicamentoDao;
+import net.daw.helper.statics.EncodingUtilHelper;
+
+
+/**
+ *
+ * @author a044888329b
+ */
+public class ViaBean implements GenericBean{
+    
+   @Expose
+    private Integer id = 0;
+   @Expose
+    private String descripcion;
+   @Expose(serialize = false)
+    private Integer id_medicamento = 0;
+    @Expose(deserialize = false)
+    private MedicamentoBean obj_medicamento;
+    
+    
+    public ViaBean() {
+        
+    }
+    
+    public ViaBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId_medicamento() {
+        return id_medicamento;
+    }
+
+    public void setId_medicamento(Integer id_medicamento) {
+        this.id_medicamento = id_medicamento;
+    }
+
+    public MedicamentoBean getObj_medicamento() {
+        return obj_medicamento;
+    }
+
+    public void setObj_medicamento(MedicamentoBean obj_medicamento) {
+        this.obj_medicamento = obj_medicamento;
+    }
+    
+    
+    
+    @Override
+    public String getColumns() {
+        String strColumns = "";
+        strColumns += "id,";
+        strColumns += "descripcion,";
+        strColumns += "id_medicamento";
+        return strColumns;
+    }
+    
+    
+    
+    @Override
+    public String getValues() {
+        String strColumns = "";
+        strColumns += id + ",";
+        strColumns += EncodingUtilHelper.quotate(descripcion) + ",";
+        strColumns += id_medicamento;
+        return strColumns;
+    }
+    
+    
+    
+    @Override
+    public String toPairs() {
+        String strColumns = "";
+//        strColumns +="id=" + id + ",";
+        strColumns += "description=" + EncodingUtilHelper.quotate(descripcion) + ",";
+        strColumns += "id_medicamento=" + id_medicamento;
+        return strColumns;
+    }
+    
+    
+    @Override
+    public ViaBean fill(ResultSet oResultSet, Connection pooledConnection, PusuarioBean oPusuarioBean_security, Integer expand) throws SQLException, Exception {
+        this.setId(oResultSet.getInt("id"));
+        this.setDescripcion(oResultSet.getString("descripcion"));
+        if (expand > 0) {
+            MedicamentoBean oMedicamentoBean = new MedicamentoBean();
+            MedicamentoDao oMedicamentoDao = new MedicamentoDao(pooledConnection, oPusuarioBean_security);
+            oMedicamentoBean.setId(oResultSet.getInt("id_medicamento"));
+            oMedicamentoBean = oMedicamentoDao.get(oMedicamentoBean, expand - 1);
+            this.setObj_medicamento(oMedicamentoBean);
+        } else {
+            this.setId_medicamento(oResultSet.getInt("id_medicamento"));
+        }
+        return this;
+    }
+    
+    
+    
+}
